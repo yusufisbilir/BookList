575496c565098e00605a60e9d89c78174da49dc7
yusufisbilir <yusufemirisbilir@gmail.com>
2021-07-05 11:39:03 +0300

    Complete book add function

--- app.js
+++ app.js
@@ -39,16 +39,40 @@ class UI{
             <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
         `;
 
-        list.appendChild(row)
+        list.appendChild(row);
+    }
+    static clearFields(){
+        document.querySelector('#title').value='';
+        document.querySelector('#author').value='';
+        document.querySelector('#isbn').value='';
     }
 }
 
 // Store class
 
 // event: display books
-document.addEventListener('DOMContentLoaded', UI.displayBooks)
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 
 // event: add a book
+document.querySelector('#book-form').addEventListener('submit', (e)=>{
+    // prevent actual submit
+    e.preventDefault();
+
+    //Get form values
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+
+    //instatiate book
+    const book = new Book(title, author, isbn);
+
+    // add book to ui
+    UI.addBookToList(book);
+
+    // clear fields
+    UI.clearFields();
+});
+
 
 // event: remove a book
